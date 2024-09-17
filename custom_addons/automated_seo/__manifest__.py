1b36a7d3e60aac5c3312b26bf17f5a69a1f3813b
DarshBac17 <darsh.shah@bacancy.com>
2024-09-17 11:15:24 +0530

    "added new snippets"

--- custom_addons/automated_seo/__manifest__.py
+++ custom_addons/automated_seo/__manifest__.py
@@ -7,6 +7,12 @@
     'author': 'Hetul Patel',
     'depends': ['base', 'web'],
     'data': [
+        "views/snippets/s_angular_header.xml",
+        "views/snippets/s_angular_heir_tag.xml",
+        "views/snippets/s_about_angular_team.xml",
+        "views/snippets/s_angular_hire_steps.xml",
+        "views/snippets/s_angular_choose_developer.xml",
+        "views/snippets/s_angular_story.xml",
         "views/snippets/s_offer.xml",
         "views/snippets/s_angular_offer.xml",
         "views/snippets/s_hire_angular_offer.xml",
@@ -16,12 +22,19 @@
         'views/snippets/snippets.xml',
     ],
     'assets': {
-            'web.assets_frontend': [
+        'web.assets_frontend': [
             'automated_seo/static/src/css/casestudy.css',
             'automated_seo/static/src/css/faq.css',
             'automated_seo/static/src/css/footer-slider.css',
             'automated_seo/static/src/css/style.css',
-            ]
+            # 'automated_seo/static/src/js/angular_story.js',
+
+        ],
+        'website.assets_wysiwyg': [
+            ('include', 'web._assets_helpers'),
+            'automated_seo/static/src/js/angular_offer.js'
+        ],
+
     },
     'installable': True,
     'application': False,
