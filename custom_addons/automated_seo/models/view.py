03f544bf101f0075b782358cbcd3751c36b2d6cd
Om Mishra <om.mishra@bacancy.com>
2024-10-16 10:55:55 +0530

    add function for upload image on server

--- custom_addons/automated_seo/models/view.py
+++ custom_addons/automated_seo/models/view.py
@@ -2,10 +2,21 @@ from docutils.nodes import image
 from lxml.html import html_parser
 from odoo import models, fields, api
 from bs4 import BeautifulSoup
+import base64
 import  html
 import base64
 import random
 import string
+import boto3
+import io
+from botocore.client import Config
+from botocore.exceptions import ClientError
+import os
+from dotenv import load_dotenv
+load_dotenv()
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
 
 class View(models.Model):
     _inherit = 'ir.ui.view'
@@ -43,26 +54,7 @@ class View(models.Model):
             })
 
     def updated_php_mapper(self, view_name):
-        # breakpoint()
-        # image_id = 268  # Replace with the ID from your image src
-        # result = self.find_changed_snippet_image(image_id)
-        # if result:
-        #     print("Changed image found:")
-        #     for key, value in result.items():
-        #         print(f"{key}: {value}")
-        # else:
-        #     print("Image not found")
-        # snippet_name = "Left Side Image Gray"
-        # page_url ='/demo-5'  # Replace with your actual page URL
-        # result = self.find_snippet_image_comprehensive(page_url=page_url, snippet_name = snippet_name)
-        # if result:
-        #     print(f"Recently replaced image for page {page_url}:")
-        #     print(f"Name: {result['name']}")
-        #     print(f"URL: {result['url']}")
-        #     print(f"Created on: {result['create_date']}")
-        #     print(f"MIME Type: {result['mimetype']}")
-        # else:
-        #     print(f"No recently replaced image found for page {page_url}")
+
         page = self.env['website.page'].search([('name', '=', view_name)], limit=1)
         html_parser = page.view_id.arch
         print(html_parser)
@@ -91,7 +83,16 @@ class View(models.Model):
                                 hash_suffix = self.generate_hash()
                                 name, ext = new_image_name.rsplit('.', 1)
                                 new_image_name = f"{name}_{hash_suffix}.{ext}"
+
                                 image_id = int(new_src.split('/')[-2].split('-')[0])
+                                attachment = self.env['ir.attachment'].search([('id', '=', image_id)])
+
+                                if attachment:
+                                    new_image_data = attachment.datas
+                                    new_image = base64.b64decode(new_image_data)
+                                    image_file = io.BytesIO(new_image)
+                                    self.upload_file_to_s3(file=image_file,s3_filename=new_image_name)
+
                                 image_path = '/'.join(new_src.split('/')[:-1])
                                 tag['src'] = image_path + '/' + new_image_name
                                 tag['data-src'] = image_path + '/' + new_image_name
@@ -105,7 +106,7 @@ class View(models.Model):
                                     'image_name': str(new_image_name)
                                 })
                                 print(self.env['ir.attachment'].search([('id', '=', image_id)]).read(['name']))
-                                self.env['ir.attachment'].search([('id', '=', image_id)]).write({
+                                attachment.write({
                                     'name': new_image_name
                                 })
                                 print(self.env['ir.attachment'].search([('id', '=', image_id)]).read(['name']))
@@ -118,142 +119,73 @@ class View(models.Model):
         wrap_tag.unwrap()
         return str(soup)
 
-    def updated_image_php_mapper(self, view_name):
-        # breakpoint()
-        # image_id = 268  # Replace with the ID from your image src
-        # result = self.find_changed_snippet_image(image_id)
-        # if result:
-        #     print("Changed image found:")
-        #     for key, value in result.items():
-        #         print(f"{key}: {value}")
-        # else:
-        #     print("Image not found")
-        # snippet_name = "Left Side Image Gray"
-        # page_url ='/demo-5'  # Replace with your actual page URL
-        # result = self.find_snippet_image_comprehensive(page_url=page_url, snippet_name = snippet_name)
-        # if result:
-        #     print(f"Recently replaced image for page {page_url}:")
-        #     print(f"Name: {result['name']}")
-        #     print(f"URL: {result['url']}")
-        #     print(f"Created on: {result['create_date']}")
-        #     print(f"MIME Type: {result['mimetype']}")
-        # else:
-        #     print(f"No recently replaced image found for page {page_url}")
-        page = self.env['website.page'].search([('name', '=', view_name)], limit=1)
-        html_parser = page.view_id.arch
-        soup = BeautifulSoup(html_parser, "html.parser")
 
-        sections = soup.find_all('section', {'data-snippet': True})
-        snippet_ids = []
-        for section in sections:
-            snippet_ids.append(section.get('data-snippet'))
-
-        for snippet_id in snippet_ids:
-            snippet_record = self.env['automated_seo.mapper'].search([('snippet_id', '=', snippet_id)], limit=1)
-
-            if snippet_record:
-                elements = snippet_record.php_tags.search(['image_name','!=', 'False',]).read(['element_class', 'php_tag', 'image_name'])
-                for element in elements:
-                    tags = soup.find_all(class_=element.get('element_class'))
-                    for tag in tags:
-                        new_src = tag.get('src')
-                        old_tag_soup = BeautifulSoup(element.get('php_tag'), 'html.parser')
-                        if new_src:
-                            new_image_name = new_src.split('/')[-1]  # Extract just the file name from the src
-                            old_img_tag = old_tag_soup.find('img')
-                            # old_img_name = old_img_tag.get('src').split('/')[-1]
-                            old_img_name = element.get('image_name')
-
-                            # temp = self.env['ir.attachment'].search([('name', '=',old_img_name )])
-                            if old_img_tag and old_img_name != new_image_name:
-                                hash_suffix = self.generate_hash()
-                                name, ext = new_image_name.rsplit('.', 1)
-                                new_image_name = f"{name}_{hash_suffix}.{ext}"
-                                image_id = new_src.split('/')[-2].split('-')[0]
-
-                                page.view_id.arch = str(soup)
-                                old_img_tag['src'] = f'path/to/images/{new_image_name}'
-
-                                old_img_tag['data-src'] = f'path/to/images/{new_image_name}'
-                                php_mapper_record = self.env['automated_seo.php_mapper'].browse(element['id'])
-                                php_mapper_record.write({
-                                    'php_tag': str(old_tag_soup),
-                                    'image_name': str(new_image_name)
-                                })
-                                self.env['ir.attachment'].search(['id','=',image_id]).write({
-                                    'name':new_image_name
-                                })
-                        tag.replace_with(old_tag_soup)
-
-        page.view_id.arch = str(soup)
-        breakpoint()
-        return page.view_id.arch
-    def php_mapper(self,html_parser, page):
-        # breakpoint()
-        # image_id = 268  # Replace with the ID from your image src
-        # result = self.find_changed_snippet_image(image_id)
-        # if result:
-        #     print("Changed image found:")
-        #     for key, value in result.items():
-        #         print(f"{key}: {value}")
-        # else:
-        #     print("Image not found")
-        # snippet_name = "Left Side Image Gray"
-        # page_url ='/demo-5'  # Replace with your actual page URL
-        # result = self.find_snippet_image_comprehensive(page_url=page_url, snippet_name = snippet_name)
-        # if result:
-        #     print(f"Recently replaced image for page {page_url}:")
-        #     print(f"Name: {result['name']}")
-        #     print(f"URL: {result['url']}")
-        #     print(f"Created on: {result['create_date']}")
-        #     print(f"MIME Type: {result['mimetype']}")
-        # else:
-        #     print(f"No recently replaced image found for page {page_url}")
-        soup = BeautifulSoup(html_parser, "html.parser")
-        sections = soup.find_all('section', {'data-snippet': True})
-        snippet_ids=[]
-        for section in sections:
-            snippet_ids.append(section.get('data-snippet'))
-
-        for snippet_id in snippet_ids:
-            snippet_record = self.env['automated_seo.mapper'].search([('snippet_id', '=', snippet_id)], limit=1)
-
-            if snippet_record:
-                elements = snippet_record.php_tags.read(['element_class', 'php_tag','image_name'])
-                for element in elements:
-                    tags = soup.find_all(class_=element.get('element_class'))
-                    for tag in tags:
-                        new_src = tag.get('src')
-                        old_tag_soup = BeautifulSoup(element.get('php_tag'), 'html.parser')
-                        if new_src:
-                            new_image_name = new_src.split('/')[-1]  # Extract just the file name from the src
-                            old_img_tag = old_tag_soup.find('img')
-                            # old_img_name = old_img_tag.get('src').split('/')[-1]
-                            old_img_name = element.get('image_name')
-
-                            # temp = self.env['ir.attachment'].search([('name', '=',old_img_name )])
-                            if old_img_tag and old_img_name!=new_image_name:
-                                hash_suffix = self.generate_hash()
-                                name, ext = new_image_name.rsplit('.', 1)
-                                new_image_name = f"{name}_{hash_suffix}.{ext}"
-                                image_id = new_src.split('/')[-2].split('-')[0]
-                                old_img_tag['src'] = f'path/to/images/{new_image_name}'
-
-                                old_img_tag['data-src'] = f'path/to/images/{new_image_name}'
-                                php_mapper_record = self.env['automated_seo.php_mapper'].browse(element['id'])
-                                php_mapper_record.write({
-                                    'php_tag': str(old_tag_soup),
-                                    'image_name':str(new_image_name)
-                                })
-
-
-                        tag.replace_with(old_tag_soup)
-
-        for tag in soup.find_all('t'):
-            tag.unwrap()
-        wrap_tag = soup.find(id="wrap")
-        wrap_tag.unwrap()
-        return  str(soup)
+    # def php_mapper(self,html_parser, page):
+    #     # breakpoint()
+    #     # image_id = 268  # Replace with the ID from your image src
+    #     # result = self.find_changed_snippet_image(image_id)
+    #     # if result:
+    #     #     print("Changed image found:")
+    #     #     for key, value in result.items():
+    #     #         print(f"{key}: {value}")
+    #     # else:
+    #     #     print("Image not found")
+    #     # snippet_name = "Left Side Image Gray"
+    #     # page_url ='/demo-5'  # Replace with your actual page URL
+    #     # result = self.find_snippet_image_comprehensive(page_url=page_url, snippet_name = snippet_name)
+    #     # if result:
+    #     #     print(f"Recently replaced image for page {page_url}:")
+    #     #     print(f"Name: {result['name']}")
+    #     #     print(f"URL: {result['url']}")
+    #     #     print(f"Created on: {result['create_date']}")
+    #     #     print(f"MIME Type: {result['mimetype']}")
+    #     # else:
+    #     #     print(f"No recently replaced image found for page {page_url}")
+    #     soup = BeautifulSoup(html_parser, "html.parser")
+    #     sections = soup.find_all('section', {'data-snippet': True})
+    #     snippet_ids=[]
+    #     for section in sections:
+    #         snippet_ids.append(section.get('data-snippet'))
+    #
+    #     for snippet_id in snippet_ids:
+    #         snippet_record = self.env['automated_seo.mapper'].search([('snippet_id', '=', snippet_id)], limit=1)
+    #
+    #         if snippet_record:
+    #             elements = snippet_record.php_tags.read(['element_class', 'php_tag','image_name'])
+    #             for element in elements:
+    #                 tags = soup.find_all(class_=element.get('element_class'))
+    #                 for tag in tags:
+    #                     new_src = tag.get('src')
+    #                     old_tag_soup = BeautifulSoup(element.get('php_tag'), 'html.parser')
+    #                     if new_src:
+    #                         new_image_name = new_src.split('/')[-1]  # Extract just the file name from the src
+    #                         old_img_tag = old_tag_soup.find('img')
+    #                         # old_img_name = old_img_tag.get('src').split('/')[-1]
+    #                         old_img_name = element.get('image_name')
+    #
+    #                         # temp = self.env['ir.attachment'].search([('name', '=',old_img_name )])
+    #                         if old_img_tag and old_img_name!=new_image_name:
+    #                             hash_suffix = self.generate_hash()
+    #                             name, ext = new_image_name.rsplit('.', 1)
+    #                             new_image_name = f"{name}_{hash_suffix}.{ext}"
+    #                             image_id = new_src.split('/')[-2].split('-')[0]
+    #                             old_img_tag['src'] = f'path/to/images/{new_image_name}'
+    #
+    #                             old_img_tag['data-src'] = f'path/to/images/{new_image_name}'
+    #                             php_mapper_record = self.env['automated_seo.php_mapper'].browse(element['id'])
+    #                             php_mapper_record.write({
+    #                                 'php_tag': str(old_tag_soup),
+    #                                 'image_name':str(new_image_name)
+    #                             })
+    #
+    #
+    #                     tag.replace_with(old_tag_soup)
+    #
+    #     for tag in soup.find_all('t'):
+    #         tag.unwrap()
+    #     wrap_tag = soup.find(id="wrap")
+    #     wrap_tag.unwrap()
+    #     return  str(soup)
 
     def remove_odoo_classes_from_tag(self, html_parser):
         soup = BeautifulSoup(html_parser, "html.parser")
@@ -289,58 +221,21 @@ class View(models.Model):
             'target': 'self',
         }
 
-    def find_snippet_image_comprehensive(self, snippet_name, page_url):
-        # Find the page
-        page = self.env['website.page'].search([('url', '=', page_url)], limit=1)
-        if not page:
-            return "Page not found"
+    def upload_file_to_s3(self, file, s3_filename):
+        s3 = boto3.client('s3',
+                          aws_access_key_id=AWS_ACCESS_KEY_ID,
+                          aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+                          )
+        try:
+            s3.upload_fileobj(file, AWS_STORAGE_BUCKET_NAME, f'Inhouse/{s3_filename}')
+            print(f"File {s3_filename} uploaded successfully to bucket {AWS_STORAGE_BUCKET_NAME}!")
+        except ClientError as e:
+            if e.response['Error']['Code'] == 'AccessDenied':
+                print("Access Denied. Please check your AWS credentials and bucket permissions.")
+            elif e.response['Error']['Code'] == 'NoSuchBucket':
+                print(f"The bucket {AWS_STORAGE_BUCKET_NAME} does not exist.")
+            else:
+                print(f"An error occurred: {e}")
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
 
-        # Check ir.attachment
-        attachment = self.env['ir.attachment'].search([
-            ('res_model', '=', 'ir.ui.view'),
-            ('res_id', '=', page.view_id.id),
-            ('mimetype', 'like', 'image/%')
-        ], order='create_date desc', limit=1)
-
-        if attachment:
-            return f"Image found in ir.attachment: {attachment.name}"
-
-        # Check view arch for background image
-        view_arch = page.view_id.arch
-        if 'background-image' in view_arch:
-            return "Image might be a CSS background image in the view arch"
-
-        # Check for custom snippet template
-        custom_snippet = self.env['ir.ui.view'].search([
-            ('name', '=', snippet_name),
-            ('type', '=', 'qweb'),
-            ('website_id', '=', page.website_id.id)
-        ], limit=1)
-
-        if custom_snippet:
-            return f"Custom snippet template found: {custom_snippet.name}"
-
-        # Check theme assets
-        theme_attach = self.env['ir.attachment'].search([
-            ('name', 'like', snippet_name),
-            ('res_model', '=', 'theme.ir.attachment')
-        ], limit=1)
-
-        if theme_attach:
-            return f"Image might be in theme assets: {theme_attach.name}"
-
-        return "Image location not found. It might be dynamically loaded or require further investigation."
-
-    def find_changed_snippet_image(self, image_id):
-        attachment = self.env['ir.attachment'].sudo().browse(image_id)
-        if attachment.exists():
-            return {
-                'id': attachment.id,
-                'name': attachment.name,
-                'mimetype': attachment.mimetype,
-                'url': attachment.url,
-                'create_date': attachment.create_date,
-                'res_model': attachment.res_model,
-                'res_id': attachment.res_id
-            }
-        return None
