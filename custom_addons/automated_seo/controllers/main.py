cf79a04cacbb683de94e205f05b4c774a024cbc7
Om Mishra <om.mishra@bacancy.com>
2024-11-04 21:18:07 +0530

    Refactor the code

--- custom_addons/automated_seo/controllers/main.py
+++ custom_addons/automated_seo/controllers/main.py
@@ -33,64 +33,17 @@ class WebsiteVersion(http.Controller):
                 return {'error': 'Associated view not found'}
 
             template_name = f"website.{url}" if url else "website.page"
-
-            # def clean_content(content):
-            #     soup = BeautifulSoup(content, 'html.parser')
-            #     # Find the <section> tag and unwrap it (remove the tag but keep its contents)
-            #     section = soup.find('main')
-            #     if section:
-            #         section.unwrap()
-            #     # Find the wrap div
-            #     wrap_div = soup.find('div', id='wrap')
-            #     if wrap_div:
-            #         # Clean up the wrap div attributes
-            #         wrap_div.attrs = {
-            #             'id': 'wrap',
-            #             'class': 'oe_structure oe_empty'
-            #         }
-            #
-            #         # Remove editor-specific attributes and classes from all elements
-            #         for element in wrap_div.find_all(recursive=True):
-            #             # Remove data-* attributes
-            #             # attrs_to_remove = [attr for attr in element.attrs if attr.startswith('data-')]
-            #             # for attr in attrs_to_remove:
-            #             #     del element[attr]
-            #
-            #             # Clean up classes
-            #             if 'class' in element.attrs:
-            #                 classes = element['class']
-            #                 classes = [c for c in classes if c not in ['o_editable', 'o_dirty']]
-            #                 if classes:
-            #                     element['class'] = classes
-            #                 else:
-            #                     del element['class']
-            #
-            #         return str(wrap_div)
-            #     return content
-
-            # Extract and clean the wrap content
-            # wrap_content = current_arch
             soup = BeautifulSoup(current_arch, 'html.parser')
-            # Find the <section> tag and unwrap it (remove the tag but keep its contents)
             section = soup.find('main')
             if section:
                 section.unwrap()
             wrap_div = soup.find('div', id='wrap')
             if wrap_div:
-                # Clean up the wrap div attributes
                 wrap_div.attrs = {
                     'id': 'wrap',
                     'class': 'oe_structure oe_empty'
                 }
-
-                # Remove editor-specific attributes and classes from all elements
                 for element in wrap_div.find_all(recursive=True):
-                    # Remove data-* attributes
-                    # attrs_to_remove = [attr for attr in element.attrs if attr.startswith('data-')]
-                    # for attr in attrs_to_remove:
-                    #     del element[attr]
-
-                    # Clean up classes
                     if 'class' in element.attrs:
                         classes = element['class']
                         classes = [c for c in classes if c not in ['o_editable', 'o_dirty']]
@@ -98,14 +51,7 @@ class WebsiteVersion(http.Controller):
                             element['class'] = classes
                         else:
                             del element['class']
-            # breakpoint()
-            # if '<div id="wrap"' in current_arch:
-            #     wrap_content = BeautifulSoup(clean_content(current_arch), 'html.parser').prettify()
             wrap_content = soup.prettify()
-            print("=================================================")
-            print(wrap_content)
-            print("=================================================")
-            # Format the final arch content
             formatted_arch = f'''<t t-name="{template_name}">
                 <t t-call="website.layout">
                     {wrap_content}
@@ -113,13 +59,7 @@ class WebsiteVersion(http.Controller):
             </t>'''
 
             if not all([name, page_id, current_arch]):
-                # version = request.env['website.page.version'].search([('status','=',True)]).write({
-                #     'view_arch': formatted_arch,
-                # })
                 return {'error': 'Missing required fields'}
-            # else:
-            # breakpoint()
-            # Create version
             version = request.env['website.page.version'].create({
                 'name': name,
                 'description': description,
@@ -137,67 +77,3 @@ class WebsiteVersion(http.Controller):
         except Exception as e:
             # _logger.exception("Error saving website version")
             return {'error': str(e)}
-
-# class WebsiteVersion(http.Controller):
-#     @http.route(['/website/version/save'], type='json', auth="user", website=True)
-#     def save_version(self, **kwargs):
-#         name = kwargs.get('name')
-#         description = kwargs.get('description')
-#         page_id = kwargs.get('page_id')
-#         current_arch = kwargs.get('current_arch')  # Get the unsaved content
-#
-#         url = page_id.split('/')[-1]
-#         page = request.env['website.page'].search([('url', '=', f'/{url}')], limit=1)
-#         view = request.env['automated_seo.view'].search([('website_page_id', '=', page.id)], limit=1)
-#
-#         if not name or not page_id or not current_arch:
-#             return {'error': 'Missing required fields'}
-#
-#         if not page.exists():
-#             return {'error': 'Page not found'}
-#
-#         version = request.env['website.page.version'].create({
-#             'name': name,
-#             'description': description,
-#             'page_id': page.id,
-#             'view_arch': current_arch,  # Use the unsaved content instead of page.view_id.arch
-#             'view_id': view.id,
-#             'user_id': request.env.user.id
-#         })
-#
-#         return {
-#             'success': True,
-#             'version_id': version.id
-#         }
-# class WebsiteVersion(http.Controller):
-#     @http.route(['/website/version/save'], type='json', auth="user", website=True)
-#     def save_version(self, **kwargs):
-#         name = kwargs.get('name')
-#         description = kwargs.get('description')
-#         page_id = kwargs.get('page_id')
-#         url = page_id.split('/')[-1]
-#         page = request.env['website.page'].search([('url', '=', f'/{url}')], limit=1)
-#         view = request.env['automated_seo.view'].search([('website_page_id', '=', page.id)], limit=1)
-#
-#
-#         # page_id = page.id
-#         if not name or not page_id:
-#             return {'error': 'Missing required fields'}
-#
-#         # page = request.env['website.page'].browse(int(page_id))
-#         if not page.exists():
-#             return {'error': 'Page not found'}
-#
-#         version = request.env['website.page.version'].create({
-#             'name': name,
-#             'description': description,
-#             'page_id': page.id,
-#             'view_arch': page.view_id.arch,
-#             'view_id':view.id,
-#             'user_id': request.env.user.id
-#         })
-#
-#         return {
-#             'success': True,
-#             'version_id': version.id
-#         }
--- custom_addons/automated_seo/models/website_version.py
+++ custom_addons/automated_seo/models/website_version.py
@@ -54,12 +54,6 @@ class WebsitePageVersion(models.Model):
             'target': 'self',
         }
 
-
-
-    # def get_formview_id(self, access_uid=None):
-    #     """Disable form view"""
-    #     return False
-
     @api.model
     def get_view(self, view_id=None, view_type='form', **options):
         """Override to prevent form view from loading"""
@@ -70,31 +64,3 @@ class WebsitePageVersion(models.Model):
 
     def write(self,vals):
         return super(WebsitePageVersion, self).write(vals)
-
-    # def fields_view_get(self, view_id=None, view_type='form', toolbar=False, submenu=False):
-    #     result = super(WebsitePageVersion, self).fields_view_get(view_id=view_id, view_type=view_type, toolbar=toolbar,
-    #                                                              submenu=submenu)
-    #     if view_type == 'form':
-    #         print("=============insdie")
-    #         # Return tree view instead of form view
-    #         return super(WebsitePageVersion, self).fields_view_get(
-    #             view_id=self.env.ref('your_module.view_version_tree').id, view_type='tree', toolbar=toolbar,
-    #             submenu=submenu)
-    #     return result
-
-# from odoo import models, fields, api
-#
-# class WebsitePageVersion(models.Model):
-#     _name = 'website.page.version'
-#     _description = 'Website Page Version'
-#     _order = 'create_date desc'
-#
-#     name = fields.Char('Version Name', required=True)
-#     description = fields.Text('Description')
-#     view_id =  fields.Many2one('automated_seo.view', string='View', required=True)
-#     page_id = fields.Many2one('website.page', string='Website Page', required=True)
-#     view_arch = fields.Text('Saved View Architecture', required=True)
-#     parse_html = fields.Text(string="Parse HTML")
-#     parse_html_binary = fields.Binary(string="Parsed HTML File", attachment=True)
-#     parse_html_filename = fields.Char(string="Parsed HTML Filename")
-#     create_uid = fields.Many2one('res.users', string='Created By', readonly=True)
